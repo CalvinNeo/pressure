0f9e700288e2b25b4d5af9e1bf801c48faa39862
CalvinNeo <calvinneo1995@gmail.com>
2023-07-07 16:27:29 +0800

    add issuer
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- pressure/src/main.rs
+++ pressure/src/main.rs
@@ -1,11 +1,15 @@
-use rand::prelude::IteratorRandom;
-use rand::Rng;
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
-use std::sync::atomic::AtomicBool;
-use std::sync::Arc;
-use std::sync::RwLock;
-use std::thread::{self, JoinHandle};
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+    sync::{atomic::AtomicBool, Arc, RwLock},
+    thread::{self, JoinHandle},
+};
+
+use clap::Parser;
+use crossbeam_channel::{bounded, select, Receiver};
+use mysql::{prelude::*, *};
+use rand::{prelude::IteratorRandom, Rng};
+use threadpool::ThreadPool;
 
 trait Sampler<T: Clone + Sync>: Send + Sync {
     fn sample(&self, n: usize) -> Vec<T>;
@@ -30,6 +34,7 @@ impl<T: Clone + Sync> Slot<T> {
     }
 
     fn sample(&self, n: usize) -> Vec<T> {
+        assert!(n <= self.items.len());
         let mut rng = rand::thread_rng();
         let v: Vec<&T> = self.items.iter().choose_multiple(&mut rng, n);
         let v: Vec<T> = v.into_iter().map(|e| e.clone()).collect();
@@ -42,17 +47,24 @@ struct Feeder<T: Clone + Sync> {
     slot_size: usize,
     slots: Vec<RwLock<Slot<T>>>,
     sampler: Arc<dyn Sampler<T>>,
+    update_millis: u64,
 }
 
 unsafe impl<T: Clone + Sync> Send for Feeder<T> {}
 
 impl<T: Clone + Sync> Feeder<T> {
-    fn new(slot_count: usize, slot_size: usize, sampler: Arc<dyn Sampler<T>>) -> Self {
+    fn new(
+        slot_count: usize,
+        slot_size: usize,
+        sampler: Arc<dyn Sampler<T>>,
+        update_millis: u64,
+    ) -> Self {
         let mut feeder = Feeder {
             slot_count,
             slot_size,
             slots: vec![],
             sampler,
+            update_millis,
         };
         for _i in 0..slot_count {
             feeder.slots.push(RwLock::new(Slot::default()));
@@ -63,9 +75,11 @@ impl<T: Clone + Sync> Feeder<T> {
         feeder
     }
 
-    fn update_cell(&self, i: usize) {
+    fn update_cell(&self, i: usize) -> usize {
         let slot = Slot::new(self.sampler.clone(), self.slot_size);
+        let size = slot.items.len();
         *self.slots[i].write().expect("update lock error") = slot;
+        size
     }
 
     fn sample(&self, n: usize) -> Vec<T> {
@@ -87,15 +101,24 @@ fn background_update<T: Clone + Sync + 'static>(
         finished.clone(),
         thread::spawn(move || {
             let finished = finished.clone();
+            let update_millis = feeder.update_millis.clone();
             let mut rng = rand::thread_rng();
             loop {
                 if finished.load(std::sync::atomic::Ordering::SeqCst) {
+                    println!("finish");
                     return;
                 }
                 let slot_id: usize = rng.gen_range(0usize..feeder.slot_count);
-                feeder.update_cell(slot_id);
-                println!("update {}", slot_id);
-                thread::sleep(std::time::Duration::from_millis(500));
+                let now = std::time::Instant::now();
+                println!("start update slot {}", slot_id);
+                let new_size = feeder.update_cell(slot_id);
+                println!(
+                    "end update slot {} elapsed millis {} new_size {}",
+                    slot_id,
+                    now.elapsed().as_millis(),
+                    new_size
+                );
+                thread::sleep(std::time::Duration::from_millis(update_millis));
             }
         }),
     )
@@ -150,40 +173,141 @@ impl Sampler<String> for PKSampler {
     }
 }
 
+struct MySQLIssuer {
+    urls: Vec<String>,
+    pools: Arc<RwLock<Vec<Pool>>>,
+    feeder: Arc<Feeder<String>>,
+    dry_run: bool,
+}
+
+impl MySQLIssuer {
+    fn new(urls: Vec<String>, feeder: Arc<Feeder<String>>, dry_run: bool) -> Self {
+        let r = Self {
+            urls: urls.clone(),
+            pools: Arc::new(RwLock::new(vec![])),
+            feeder,
+            dry_run,
+        };
+        if !dry_run {
+            for url in urls.iter() {
+                let pool = Pool::new(url.as_str()).unwrap();
+                r.pools.write().expect("update lock").push(pool);
+            }
+        }
+        r
+    }
+
+    fn start(&self, n_threads: usize) -> (Arc<AtomicBool>, ThreadPool) {
+        let thread_pool = ThreadPool::with_name("issuer".into(), n_threads);
+        let finished = Arc::new(AtomicBool::new(false));
+        for thid in 0..n_threads {
+            let finished = finished.clone();
+            let thread_id = thid.clone();
+            let urls = self.urls.clone();
+            let pools = self.pools.clone();
+            let feeder = self.feeder.clone();
+            let dry_run = self.dry_run.clone();
+            thread_pool.execute(move || {
+                let mut count = 0;
+                loop {
+                    if finished.load(std::sync::atomic::Ordering::SeqCst) {
+                        break;
+                    }
+                    let mut rng = rand::thread_rng();
+                    let tidb_id: usize = rng.gen_range(0usize..urls.len());
+                    // Let's create a table for payments.
+                    let random_string: String = (0..5).map(|_| rng.gen_range(b'a'..=b'z') as char).collect();
+                    let billcode = feeder.sample(1).into_iter().nth(0).unwrap();
+                    let s = format!("update rtdb.zto_ssmx_bill_detail set forecast_stat_day = '{}' where bill_code='{}'", random_string, billcode);
+                    if dry_run {
+                        println!("thread_id {} tidb_id {} sql {}", thread_id, tidb_id, s);
+                    } else {
+                        let mut conn = pools.read().expect("read lock")[tidb_id].get_conn().unwrap();
+                        conn.query_drop(s).unwrap();
+                    }
+                    count += 1;
+                    if count % 10000 == 0 {
+                        println!("thread_id {} finished {}", thread_id, count);
+                    }
+                }
+            });
+        }
+        (finished, thread_pool)
+    }
+}
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct PKIssueArgs {
+    /// TiDB endpoints splitted by `,`, like mysql://root@127.0.0.1:4000/
+    #[arg(short, long)]
+    tidb_addrs: String,
+
+    /// Input files splitted by `,`.
+    #[arg(short, long)]
+    input_files: String,
+
+    /// MySQL workers.
+    #[arg(short, long, default_value_t = 6)]
+    workers: usize,
+
+    /// Interval to update a random slot.
+    #[arg(long, default_value_t = 10000)]
+    update_interval_millis: u64,
+
+    /// Perform dry run
+    #[arg(long, default_value_t = false)]
+    dry_run: bool,
+
+    /// How many slots.
+    #[arg(long, default_value_t = 100)]
+    slot_count: usize,
+
+    /// How many items in one slot.
+    #[arg(short, long)]
+    slot_size: usize,
+}
+
+fn ctrl_channel() -> Result<Receiver<()>, ctrlc::Error> {
+    let (sender, receiver) = bounded(100);
+    ctrlc::set_handler(move || {
+        let _ = sender.send(());
+    })?;
+
+    Ok(receiver)
+}
+
+/// ./target/debug/pressure --dry-run --tidb-addrs mysql://root@127.0.0.1:4000/ --input-files /Users/calvin/pressure/pressure/a,/Users/calvin/pressure/pressure/b,/Users/calvin/pressure/pressure/c -s 1000 --update-interval-millis 100
 fn main() {
-    let pk = PKSampler {
-        file_names: vec![
-            "/Users/calvin/pressure/pressure/a".to_string(),
-            "/Users/calvin/pressure/pressure/b".to_string(),
-            "/Users/calvin/pressure/pressure/c".to_string(),
-        ],
-    };
-    let feeder: Arc<Feeder<String>> = Arc::new(Feeder::new(10, 4, Arc::new(pk)));
+    let args = PKIssueArgs::parse();
+    let file_names = args.input_files.split(",").map(|e| e.to_string()).collect();
+    let pk = PKSampler { file_names };
+    let feeder: Arc<Feeder<String>> = Arc::new(Feeder::new(
+        args.slot_count,
+        args.slot_size,
+        Arc::new(pk),
+        args.update_interval_millis,
+    ));
     let (f, j) = background_update(feeder.clone());
     let feed = feeder.clone();
-    let j1 = thread::spawn(move || loop {
-        for i in feed.sample(3).into_iter() {
-            println!("res1 {}", i);
+
+    let addrs: Vec<String> = args.tidb_addrs.split(",").map(|e| e.to_string()).collect();
+    let iss: MySQLIssuer = MySQLIssuer::new(addrs, feed, args.dry_run);
+    let (f2, thread_pool) = iss.start(args.workers);
+
+    let ctrl_c_events = ctrl_channel().unwrap();
+    loop {
+        select! {
+            recv(ctrl_c_events) -> _ => {
+                println!("Goodbye!");
+                f.store(true, std::sync::atomic::Ordering::SeqCst);
+                f2.store(true, std::sync::atomic::Ordering::SeqCst);
+                j.join().unwrap();
+                thread_pool.join();
+                println!("Quit!");
+                break;
+            }
         }
-        thread::sleep(std::time::Duration::from_millis(500));
-    });
-    let feed = feeder.clone();
-    let j2 = thread::spawn(move || loop {
-        for i in feed.sample(3).into_iter() {
-            println!("res2 {}", i);
-        }
-        thread::sleep(std::time::Duration::from_millis(500));
-    });
-    let feed = feeder.clone();
-    let j3 = thread::spawn(move || loop {
-        for i in feed.sample(3).into_iter() {
-            println!("res3 {}", i);
-        }
-        thread::sleep(std::time::Duration::from_millis(500));
-    });
-    f.store(true, std::sync::atomic::Ordering::SeqCst);
-    j.join().unwrap();
-    j1.join();
-    j2.join();
-    j3.join();
+    }
 }
